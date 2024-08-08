ec4cd800d798608c7f8376948ca8bd3bd34d8d7f
Umesh Prajapati <Umesh.Prajapati@godigit.com>
2024-08-08 15:01:08 +0530

    function to access data from json added

--- build/service/searchProduct.js
+++ build/service/searchProduct.js
@@ -1,16 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const product_1 = require("./product");
+const db_json_1 = require("../db/db.json");
 var read = require("../../node_modules/readline-sync");
 // dynamic serach
 // exception handling
 // json (try)
 const producToSearch = read.question("Enter Product Name to delete :");
+let product = db_json_1.productList;
+//console.log(product);
 var check = 1;
-for (var i = 0; i < product_1.productList.length; i++) {
-    if (product_1.productList[i].getProductName().toLocaleLowerCase() === producToSearch.toLocaleLowerCase()) {
-        console.log("Product Found:");
-        product_1.productList.splice(i, 1);
+for (var i = 0; i < db_json_1.productList.length; i++) {
+    if (db_json_1.productList[i].productName.toLocaleLowerCase() === producToSearch.toLocaleLowerCase()) {
+        console.log("Product Found !");
+        db_json_1.productList.splice(i, 1);
+        console.log("Product Deleted !");
         check = 0;
         break;
     }
@@ -20,10 +23,10 @@ if (check) {
 }
 function DisplayProduct() {
     console.log("Updated List of Products  :");
-    for (var i = 0; i < product_1.productList.length; i++) {
-        console.log("Product Name:", product_1.productList[i].getId());
-        console.log("Product Name:", product_1.productList[i].getProductName());
-        console.log("Product Name:", product_1.productList[i].getPrice());
+    for (var i = 0; i < db_json_1.productList.length; i++) {
+        console.log("Product Name:", db_json_1.productList[i].id);
+        console.log("Product Name:", db_json_1.productList[i].productName);
+        console.log("Product Name:", db_json_1.productList[i].price);
     }
 }
 DisplayProduct();
--- src/service/searchProduct.ts
+++ src/service/searchProduct.ts
@@ -1,14 +1,17 @@
-import { productList } from "./product";
+import { Product } from "../model/index";
+import{productList } from"../db/db.json"
 var read = require("../../node_modules/readline-sync");
 // dynamic serach
 // exception handling
 // json (try)
 const producToSearch = read.question("Enter Product Name to delete :")
+let product = productList;
+//console.log(product);
 var check = 1;
 for (var i = 0; i <productList.length; i++) {
-    if (productList[i].getProductName().toLocaleLowerCase() === producToSearch.toLocaleLowerCase()) {
+    if (productList[i].productName.toLocaleLowerCase() === producToSearch.toLocaleLowerCase()) {
         console.log("Product Found !");
-        productList.splice(i, 1);
+         productList.splice(i, 1);
         console.log("Product Deleted !");
         check = 0;
         break;
@@ -23,9 +26,9 @@ function DisplayProduct()
 {
     console.log("Updated List of Products  :");
     for (var i = 0; i < productList.length; i++) {
-        console.log("Product Name:", productList[i].getId());
-        console.log("Product Name:", productList[i].getProductName());
-        console.log("Product Name:", productList[i].getPrice());
+        console.log("Product Name:", productList[i].id);
+        console.log("Product Name:", productList[i].productName);
+        console.log("Product Name:", productList[i].price);
     }
 }
 DisplayProduct();
